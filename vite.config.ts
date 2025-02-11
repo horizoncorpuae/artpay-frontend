4874f507d64a7812a84f9fdf99d0088856ba6e24
giacomosx <giacomo.bartoli@me.com>
2025-02-11 15:26:25 +0100

    update robots.txt + vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import fs from "fs";
 
 // https://vitejs.dev/config/
 
@@ -22,10 +23,10 @@ export default defineConfig({
           return path;
         }
       }
-    }
-    /*https: {
+    },
+    https: {
       key: fs.readFileSync("./local_certs/artpay.art-key.pem"),
       cert: fs.readFileSync("./local_certs/artpay.art.pem")
-    }*/
+    }
   }
 });
