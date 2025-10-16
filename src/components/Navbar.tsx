7af04faa76e60067d7635f83a31a97ecc5c65268
giacomosx <giacomo.bartoli@me.com>
2025-10-16 17:24:02 +0200

    add gallerie in menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,6 +12,7 @@ type MenuLinks = {
 
 const menuLinks: MenuLinks[] = [
   { label: "Chi siamo", href: "/chi-siamo", requireAuth: false },
+  { label: "Gallerie", href: "/gallerie", requireAuth: false },
   { label: "Guide", href: "/guide", requireAuth: false },
 ];
 
